6beab46a9ebcddeedbf328627312c74075578ffd
Darek Danielewski <dydek@hotmail.com>
2019-08-26 15:42:27 -0500

    Before refactoring to multilinemessages

--- NLog.Targets.Http/HTTP.cs
+++ NLog.Targets.Http/HTTP.cs
@@ -1,63 +1,99 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
 using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NLog.Common;
 using NLog.Config;
 
 namespace NLog.Targets.Http
 {
     [Target("HTTP")]
-    public class HTTP:TargetWithLayout
+    public class HTTP : TargetWithLayout
     {
-        [RequiredParameter] 
-        public string URL { get; set; }
+        readonly ConcurrentQueue<Action> _taskQueue = new ConcurrentQueue<Action>();
+        readonly CancellationTokenSource _terminateProcessor = new CancellationTokenSource();
+        HttpWebRequest http;
+
+        [RequiredParameter] public string URL { get; set; }
 
         public string Authorization { get; set; }
 
         public bool IgnoreSslErrors { get; set; } = true;
- 
-        protected override void Write(LogEventInfo logEvent) 
+
+
+        protected override void InitializeTarget()
         { 
-            string logMessage = this.Layout.Render(logEvent); 
+            http = (HttpWebRequest) WebRequest.Create(URL);
+            http.KeepAlive = false;
+            http.Method = "POST";
+            if (IgnoreSslErrors)
+                http.ServerCertificateValidationCallback = (sender, certificate, chain, errors) => true;
+            if (!string.IsNullOrWhiteSpace(Authorization)) http.Headers.Add("Authorization", Authorization);
 
-            SendTheMessageToRemoteHost(this.URL, logMessage); 
-        } 
- 
-        private void SendTheMessageToRemoteHost(string url, string message)
-        {
-            HttpWebRequest req = WebRequest.CreateHttp(url);
-            
-            //TODO
-            //req.Proxy = new System.Net.WebProxy(ProxyString, true);
-
-            if(IgnoreSslErrors)
-            req.ServerCertificateValidationCallback = (sender, certificate, chain, errors) => true;
-            
-            //TODO
-            req.ContentType = "application/json";
-            req.KeepAlive = false;
-            if (!String.IsNullOrWhiteSpace(Authorization))
-            {
-                req.Headers.Add("Authorization", Authorization);
-            }
-            req.Method = "POST";
-            //We need to count how many bytes we're sending. Post'ed Faked Forms should be name=value&
-            byte [] bytes = System.Text.Encoding.ASCII.GetBytes(message);
-            req.ContentLength = bytes.Length;
-            using (System.IO.Stream os = req.GetRequestStream())
-            {
-                os.Write (bytes, 0, bytes.Length); //Push it out there
-                os.Close ();
-            }
-
-            req.GetResponseAsync().ContinueWith(resp =>
-            {
-                using (System.IO.StreamReader sr = new System.IO.StreamReader(resp.Result.GetResponseStream()))
+            Task.Factory.StartNew(() =>
                 {
-                    //TODO
-                    var result = sr.ReadToEnd().Trim();
-                    //Common.InternalLogger.Warn(ex, "SplunkHttpEventCollector: Failed to lookup {0}", lookupType);
-                }
-            });
+                    while (!_terminateProcessor.IsCancellationRequested)
+                    {
+                        while (!_taskQueue.IsEmpty)
+                        {
+                            Action action = null;
+                            _taskQueue.TryDequeue(out action);
+                            action?.Invoke();
+                        }
+                        Thread.Sleep(1);
+                    }
+                }, _terminateProcessor.Token, TaskCreationOptions.AttachedToParent | TaskCreationOptions.LongRunning,
+                TaskScheduler.Default);
+            base.InitializeTarget();
+        }
 
-        } 
+        protected override void CloseTarget()
+        {
+            _terminateProcessor.Cancel(false);
+            base.CloseTarget();
+        }
+
+        protected override void FlushAsync(AsyncContinuation asyncContinuation)
+        {
+            while (!_taskQueue.IsEmpty)
+            {
+                Thread.Sleep(1);
+            }
+            base.FlushAsync(asyncContinuation);
+        }
+
+        protected override void Write(LogEventInfo logEvent)
+        {
+            _taskQueue.Enqueue(() =>
+            {
+                var logMessage = Layout.Render(logEvent);
+                SendFast(URL, logMessage);
+            });
+        }
+
+        private void SendFast(string url, string message)
+        {
+            var bytes = Encoding.ASCII.GetBytes(message);
+            http.ContentLength = bytes.Length;
+            using (var os = http.GetRequestStream())
+            {
+                os.Write(bytes, 0, bytes.Length); //Push it out there
+                os.Close();
+            }
+
+            using (var response = http.GetResponseAsync())
+            {
+                using (var stream = response.Result.GetResponseStream())
+                {
+                    using (var sr = new StreamReader(stream))
+                    {
+                        var content = sr.ReadToEnd();
+                    }
+                }
+            }
+        }
     }
 }
--- UnitTests-ConfigurationAndLayout/SplunkTests.cs
+++ UnitTests-ConfigurationAndLayout/SplunkTests.cs
@@ -8,24 +8,30 @@ namespace UnitTests_ConfigurationAndLayout
     [TestFixture]
     public class SplunkTests
     {
-        private ILogger _logger = null;
         [SetUp]
         public void TestSetUp()
         {
             _logger = LogManager.GetCurrentClassLogger();
         }
 
-        [Test]
-        public void LogTests()
+        [TearDown]
+        public void TestShutdown()
         {
-            _logger.Info($"{DateTime.Now} - Confirm visually " );
+            LogManager.Flush();
         }
+
+        private ILogger _logger;
 
         [Test]
         public void LoadTest()
         {
-            Parallel.For(0, 100, i => _logger.Info($"{i} at {DateTime.Now}"));
+            Parallel.For(0, 10, i => _logger.Info($"{i} at {DateTime.Now}"));
         }
 
+        [Test]
+        public void LogTests()
+        {
+            _logger.Info($"{DateTime.Now} - Confirm visually ");
+        }
     }
 }
