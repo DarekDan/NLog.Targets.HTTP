193541232c8a8fefb4b6ea84f9a4acdddc9331e6
Darek Danielewski <dydek@hotmail.com>
2019-08-29 21:18:33 -0500

    NuGet Seed

--- NugetPackager/Properties/VersionInfo.cs
+++ NugetPackager/Properties/VersionInfo.cs
@@ -1,10 +1,10 @@
 using System.Reflection;
 
 // This should be the same version as below
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.0.0-PreRelease")]
+[assembly: AssemblyInformationalVersion("0.0.1-PreRelease")]
 #else
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.1")]
 #endif
