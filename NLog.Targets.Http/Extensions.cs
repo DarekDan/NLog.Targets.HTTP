a8a30bf52c25f033b2490506a7b16f46de383777
Dariusz Danielewski <dydek@hotmail.com>
2022-01-02 12:05:10 -0500

    Missed to add Extensions

---
+++ NLog.Targets.Http/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace NLog.Targets.Http
+{
+    public static class Extensions
+    {
+        public static void Append(this MemoryStream memoryStream, byte[] arrayBytes)
+        {
+            memoryStream.Write(arrayBytes, 0, arrayBytes.Length);
+        }
+    }
+}
